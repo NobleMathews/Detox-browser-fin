2f8af8107a51d62a1cf71a713fcd3a65964365b7
Noble Mathews <55821103+NobleMathews@users.noreply.github.com>
2021-05-14 17:50:20 +0530

    News bef Vid

--- scripts/remover.js
+++ scripts/remover.js
@@ -100,7 +100,9 @@ function getText(domElement) {
         constants: {
             queries: {
                 result_links: '.g div > a[href*="http"]', 
-                link_parent_node: '#rso div.g', 
+                story_links: 'g-inner-card div > a[href*="http"]', 
+                link_parent_node: '#rso div.g',
+                link_story_node: '#rso g-inner-card', 
                 main_google_node: 'main'
             }, 
             events: {
@@ -130,7 +132,9 @@ function getText(domElement) {
             });
         }, 
         getAllLinks: function() {
-            return document.querySelectorAll(this.constants.queries.result_links);
+            normy_links = document.querySelectorAll(this.constants.queries.result_links);
+            story_links = document.querySelectorAll(this.constants.queries.story_links);
+            return  Array.prototype.concat.call(...normy_links , ...story_links );
         }, 
         remove: function(info) {
             var tId = info.tId;
@@ -140,6 +144,9 @@ function getText(domElement) {
                 var isAccordionItem = Boolean(link.closest('g-accordion-expander'))
                 var isHugeCardOnTheRight = Boolean(link.closest('#wp-tabs-container'))
                 var isFeatured = Boolean(link.closest(".xpdopen"))
+                // if(Boolean(link.closest('g-inner-card'))){
+                //     console.log(link);
+                // }
                 // .ULSxyf
                 return !isAccordionItem && !isHugeCardOnTheRight && !isFeatured;
             });
@@ -172,7 +179,7 @@ function getText(domElement) {
             var wId = info.wId;
             return this.currentUrl[wId][tId] === currentUrl;
         }, 
-        addClientRectsOverlay: function(elt,sentimenti) {
+        addClientRectsOverlay: function(elt,sentimenti,sentimenti2) {
             /* Absolutely position a div over each client rect so that its border width
                is the same as the rectangle's width.
                Note: the overlays will be out of place if the user resizes or zooms. */
@@ -188,14 +195,28 @@ function getText(domElement) {
               var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
               var scrollLeft = document.documentElement.scrollLeft || document.body.scrollLeft;
               tableRectDiv.style.margin = '0px';
-              tableRectDiv.style.padding = '5px 25px';
+              if(elt.tagName=='DIV')
+              {
+                tableRectDiv.style.padding = '5px 25px';
+                tableRectDiv.style.marginBlockEnd = "10px";
+              }
+              else if(elt.tagName=="G-INNER-CARD"){
+                tableRectDiv.style.padding = '0px 25px';
+                tableRectDiv.style.marginBlockEnd = "0px";
+              }
+
+
               tableRectDiv.style.top = (rect.top + scrollTop) + 'px';
               tableRectDiv.style.left = (rect.left + scrollLeft) + 'px';
               // We want rect.width to be the border width, so content width is 2px less.
-              tableRectDiv.style.width = (rect.width - 2) + 'px';
-              tableRectDiv.style.minHeight = (rect.height - 2) + 'px';
+              tableRectDiv.style.width = (rect.width) + 'px'; //-2
+              tableRectDiv.style.minHeight = (rect.height) + 'px'; // -2
+              
               tableRectDiv.innerHTML=sentimenti.outerHTML;
-              tableRectDiv.style.marginBlockEnd = "10px";
+              if(this.tagName=='G-INNER-CARD'){
+                tableRectDiv.innerHTML=sentimenti2.outerHTML;
+            }
+
               tableRectDiv.onclick = function(){
                 that.ignoreList.push(original.id);
                 this.replaceWith(original);
@@ -207,15 +228,24 @@ function getText(domElement) {
                 return tableRectDiv
             }
         },
-        getData: async function(text,overlay,score) {
+        getData: async function(text,overlay,score, type, href="") {
             const URL = 'https://services.iittp.ac.in/detoxbrowser/';
             // const URL = 'http://localhost:3000/';
-            var b = text.replace(/(?:https?|ftp?|www.):\/\/[\n\S]+/g, '');
-            const textf = document.createElement('p');
-            const textc = parse(text.match(/(?:https?|ftp?|www.):\/\/[\n\S]+/)[0]);
+            var b, textf, textc;
+            textf = document.createElement('p');
+            b = text.replace(/(?:https?|ftp?|www.):\/\/[\n\S]+/g, '');
 
-            textf.innerHTML= textc.domain; 
-            overlay.appendChild(textf);
+            if(type==0){
+                let check_ref = text.match(/(?:https?|ftp?|www.):\/\/[\n\S]+/);
+                if(check_ref){
+                    textc = parse(check_ref[0]);
+                    textf.innerHTML= textc.domain; 
+                }
+            }
+            else if(type==1){
+                textf.innerHTML = `${parse(href).domain} \n`;
+            }
+
             const data = JSON.stringify({"text":`${b}`})
             const keyb = [/JavaScript/i,
             /Python/i,
@@ -243,9 +273,10 @@ function getText(domElement) {
                 }
                   const text = document.createElement('p');
                   text.innerHTML=`🤖[${score}] ${data.categoriser} \n`;
-                  const text2 = document.createElement('p')
+                  const text2 = document.createElement('p');
                   text2.className = "spoiler";
                   text2.innerHTML= joint;
+                  overlay.appendChild(textf);
                   overlay.appendChild(text);
                   overlay.appendChild(text2);
                 }
@@ -253,16 +284,18 @@ function getText(domElement) {
         },
         deleteOldGrandpaNode: async function(el) {
             var parent = el.closest(this.constants.queries.link_parent_node);
+            if(!parent) parent = el.closest(this.constants.queries.link_story_node);
             if(!parent) return console.log(this.constants.console.needs_to_be_updated);
             // console.log(parent.getBoundingClientRect());
             // this.addClientRectsOverlay(parent);
             // const original = parent;
             if(this.ignoreList.includes(parent.id)) return;
-
             // "js": [ "scripts/jquery-3.6.0.min.js", "scripts/remover.js" ],
             // console.log(jQuery(parent).text());
             let check_text = getText(parent);
-            const re = /(definition|dictionary|wiki)/i
+            const linkRx = /<a\s+(?:[^>]*?\s+)?href=(["'])(.*?)\1/;
+            let href_text = parent.innerHTML.match(linkRx)[2]||"";
+            const re = /(definition|dictionary|wiki|britannica)/i
             if(((check_text || '').match(re) || []).length>0){
                 return;
             }
@@ -285,14 +318,24 @@ function getText(domElement) {
                     img = chrome.runtime.getURL('images/emote/0.png');                              
             }
             let sentimenti = this.createThumbnail(img);
+            let sentimenti2 = this.createThumbnail2(img);
             var overlay;
             if(score<0){
-                overlay = this.addClientRectsOverlay(parent,sentimenti);
-                this.getData(check_text,overlay,score);
+                overlay = this.addClientRectsOverlay(parent,sentimenti,sentimenti2);
+                if(parent.tagName=='DIV'){
+                    this.getData(check_text,overlay,score, 0);
+                }
+                else if(parent.tagName=='G-INNER-CARD'){
+                    this.getData(check_text,overlay,score, 1, href_text)
+                }
                 parent.replaceWith(overlay)
             }
-            parent.insertBefore(sentimenti,parent.firstChild);
-            // parent.style.display = 'none';
+            if(parent.tagName=='DIV'){
+                parent.insertBefore(sentimenti,parent.firstChild);
+            }
+            if(parent.tagName=='G-INNER-CARD'){
+                parent.insertBefore(sentimenti2,parent.firstChild);
+            }
 
         },
         createThumbnail: function(href) {
@@ -310,6 +353,22 @@ function getText(domElement) {
             thumb.style.border = "none";
             thumb.style.position = "absolute";
             thumb.style.left = "-64px";
+
+            return thumb;
+        },
+        createThumbnail2: function(href) {
+            var thumb;					
+            thumb = document.createElement("img");
+			thumb.setAttribute("loading", "lazy");	
+            thumb.setAttribute("align", "left");	
+            thumb.setAttribute("src", href);
+            thumb.style.width = "32px";
+            thumb.style.height = "32px";
+			thumb.setAttribute("width", 32);
+			thumb.setAttribute("height", 32);
+            thumb.style.backgroundPosition = "center";
+            thumb.style.backgroundRepeat = "no-repeat";
+            thumb.style.border = "none";
 
             return thumb;
         },
